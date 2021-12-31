a87aded348f3c90f7bdccce77ec3a1c45bd97d90
aymanab5 <aymansce@gmail.com>
2021-12-31 02:12:21 +0200

    add useEffect to Banner Comp

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -2,6 +2,19 @@ import React, { useState } from "react";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.lesngth - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
   return <header></header>;
 }
 
